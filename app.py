01324f95a8c2b3a82b130022db9b3867a44efc25
russiantech <chrisjsmez@gmail.com>
2024-12-17 10:09:04 +0100

    sewa recipe

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+# Database initialization
+with app.app_context():
+    db.create_all()
+    
 # Models
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -79,11 +83,6 @@ def order_recipe(id):
 def order_history():
     history = OrderHistory.query.join(Recipe).add_columns(Recipe.name, OrderHistory.timestamp).all()
     return render_template('history.html', history=history)
-
-# Database initialization
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
